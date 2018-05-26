5527faf7e9ec8f0b2f93ac711291a969cef288b2
Ángel Berhó <bergraan@gmail.com>
2018-05-26 16:49:34 +0200

    Added another overwrite

--- project/serializers/login_custom_serializer.py
+++ project/serializers/login_custom_serializer.py
@@ -28,3 +28,17 @@ class LoginSerializerCustom(LoginSerializer):
             raise exceptions.ValidationError(msg)
 
         return user
+
+    def _validate_username_email(self, username, email, password):
+        user = None
+        request = self.context.get('request')
+
+        if email and password:
+            user = authenticate(request=request, email=email, password=password)
+        elif username and password:
+            user = authenticate(request=request, username=username, password=password)
+        else:
+            msg = _('Must include either "username" or "email" and "password".')
+            raise exceptions.ValidationError(msg)
+
+        return user
